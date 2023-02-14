30eea9412e51b0cb28574d4784e442b6c12d26e5
Jameer Basha sheik <123353929+Jameer-Basha@users.noreply.github.com>
2023-02-14 15:37:19 +0530

    Create Super Reduce String.py

---
+++ February/11-02-2023/Super Reduce String.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superReducedString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def superReducedString(s):
+    # Write your code here
+    i=0
+    while(i<len(s)-1):
+        if(s[i]==s[i+1]):
+            s=s[:i]+s[i+2:]
+            i=0
+        else:
+            i+=1
+    if(len(s)==0):
+        return "Empty String"
+    else:
+        return s
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = superReducedString(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
